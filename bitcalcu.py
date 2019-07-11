0efc27e89fdb3bd69be5b0c659efbfff18dda2ca
eatonjosh <josheaton2004@gmail.com>
2019-07-11 12:10:07 +0100

    the code
    updated the variables to make more sense and added rounding for profit

--- bitcalcu.py
+++ bitcalcu.py
@@ -6,21 +6,22 @@ print("Mode 2 is for hypophetical profit calculations")
 mode = input("Mode 1 or 2?")
 
 if mode == '1':
-  original_number = float(input("I brought bitcoin when 1BTC cost:£"))# buy price
+  buy_price = float(input("I brought bitcoin when 1BTC cost:£"))# original price
 elif mode == '2':
-    original_number = float(input("If I brought bitcoin when 1BTC cost:£"))
+    buy_price = float(input("If I brought bitcoin when 1BTC cost:£"))
 if mode == '1':
-    new_number = (r.json()['bpi']['GBP']['rate_float']) # current price
+    current_price = (r.json()['bpi']['GBP']['rate_float']) # new number
 elif mode == '2':
-    new_number = float(input("And now 1BTC is worth this:£"))
+    current_price = float(input("And now 1BTC is worth this:£"))
     
-new_number = round(new_number,2)
-increase = new_number - original_number
-percentage_increase = increase / original_number * 100
+current_price = round(current_price,2)
+increase = current_price - buy_price
+percentage_increase = increase / buy_price * 100
 money_invested = float(input("And I invested £"))  # money invested
 new_price = money_invested + (money_invested*percentage_increase/100)
 new_price = round(new_price,2)
 profit = new_price - money_invested
+profit = round(profit,2)
 
 if mode == '1':
     print("It is now be worth: £",new_price)
@@ -29,7 +30,7 @@ if mode == '2':
     print("It would now be worth: £",new_price)
     print("And that would be £",profit,"profit")
 if mode == '1':
-    print(new_number," is the current price of BTC")
+    print(current_price," is the current price of BTC")
 
     
     
