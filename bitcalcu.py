2a529621495099fb306401518ba12416fc5b9df9
eatonjosh <josheaton2004@gmail.com>
2019-07-14 23:04:44 +0100

    api needs fixing

--- bitcalcu.py
+++ bitcalcu.py
@@ -1,5 +1,8 @@
 import requests
-r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+import time
+ccprices = requests.get('https://api.nomics.com/v1/currencies/ticker?key=2018-09-demo-dont-deploy-b69315e440beb145&ids=BTC,ETH,BCH&interval=1d,30d&convert=GBP')
+print(ccprices.json())
+print(ccprices)
 
 print("Mode 1 is for real profit calculations")
 print("(If I brought BTC when it cost £x, how much would my investment be")
@@ -9,12 +12,28 @@ print("Mode 2 is for hypophetical profit calculations")
 print("(If I brought BTC when it cost £x, and BTC could be worth £x, how")
 print("much would my investment be worth now?)")
 
-
 mode = input("Mode 1 or 2?")
+if mode == '1':
+    print("")
+elif mode == '2':
+    print("")
+else:
+    print("Error: Input 1 or 2")
+    time.sleep(3)
+    quit()
+
+crypto = input("BTC, BCH or ETH?:")
+if crypto == 'BTC' or 'BCH' or 'ETH':
+    print("")
+else:
+    print("Error: Choose BTC, BCH or ETH")
+    time.sleep(3)
+    quit()
 
 if mode == '1':
-    buy_price = float(input("I brought bitcoin when 1BTC cost:£"))  # original price
-    current_price = (r.json()['bpi']['GBP']['rate_float'])  # new number
+    buy_price = float(input("I brought " + crypto + " when 1" + crypto + " cost:£"))  # original price
+    if crypto == 'BTC':
+        current_price = (ccprices.json()['BTC']['price'])  # new number
 elif mode == '2':
     buy_price = float(input("If I brought bitcoin when 1BTC cost:£"))
     current_price = float(input("And 1BTC could be worth this:£"))
@@ -38,5 +57,3 @@ if mode == '2':
     print("The investment would now be worth: £", new_price)
     print("And that would be £", profit, "profit")
 input("")
-
-# python C:\Users\joshe\Documents\bitcalcu.py
