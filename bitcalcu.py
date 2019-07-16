bd8f1e34e128c3a74631f840e80c14e4348c33f1
eatonjosh <josheaton2004@gmail.com>
2019-07-16 23:48:29 +0100

    finally finished the API problems :) ready to merge

--- bitcalcu.py
+++ bitcalcu.py
@@ -1,6 +1,9 @@
 import requests
 import time
-ccprices = requests.get('https://api.coinmarketcap.com/v1/ticker/?convert=GBP')
+url = 'https://api.coinmarketcap.com/v1/ticker/?convert=GBP'
+btc_price_gbp = (requests.get(url).json()[0]["price_gbp"])
+eth_price_gbp = (requests.get(url).json()[1]["price_gbp"])
+bch_price_gbp = (requests.get(url).json()[3]["price_gbp"])
 
 
 print("Mode 1 is for real profit calculations")
@@ -21,8 +24,8 @@ else:
     time.sleep(3)
     quit()
 
-crypto = input("BTC, BCH or ETH?:")
-if crypto == 'BTC' or 'BCH' or 'ETH':
+crypto_choice = input("BTC, BCH or ETH?:")
+if crypto_choice == 'BTC' or 'BCH' or 'ETH':
     print("")
 else:
     print("Error: Choose BTC, BCH or ETH")
@@ -30,13 +33,18 @@ else:
     quit()
 
 if mode == '1':
-    buy_price = float(input("I brought " + crypto + " when 1" + crypto + " cost:£"))  # original price
-    if crypto == 'BTC':
-        current_price = (ccprices.json()['BTC']['price'])  # new number
+    buy_price = float(input("I brought " + crypto_choice + " when 1" + crypto_choice + " cost:£"))  # original price
+    if crypto_choice == 'BTC':
+        current_price_int = btc_price_gbp  # new number
+    elif crypto_choice == 'BCH':
+        current_price_int = bch_price_gbp
+    elif crypto_choice == 'ETH':
+        current_price_int = eth_price_gbp
 elif mode == '2':
-    buy_price = float(input("If I brought bitcoin when 1BTC cost:£"))
-    current_price = float(input("And 1BTC could be worth this:£"))
+    buy_price = float(input("I brought " + crypto_choice + " when 1" + crypto_choice + " cost:£"))
+    current_price_int = float(input("And 1" + crypto_choice + " could be worth this:£"))
 
+current_price = float(current_price_int)
 current_price = round(current_price, 2)
 increase = current_price - buy_price
 percentage_increase = increase / buy_price * 100
@@ -48,10 +56,12 @@ new_price = round(new_price, 2)
 profit = new_price - money_invested
 profit = round(profit, 2)
 
+print("")
+
 if mode == '1':
     print("The investment would now be worth: £", new_price)
     print("And the profit would be: £", profit)
-    print("£", current_price, " is the current price of BTC")
+    print("£", current_price, " is the current price of " + crypto_choice)
 if mode == '2':
     print("The investment would now be worth: £", new_price)
     print("And that would be £", profit, "profit")
